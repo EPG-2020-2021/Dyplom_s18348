b9891babb13e1192174abd1588fb9165122002ee
Tnxah <tnxah.j@gmail.com>
2021-10-19 22:45:22 +0200

    blabla

--- Plague/Assets/Scripts/CraftTableGUI.cs
+++ Plague/Assets/Scripts/CraftTableGUI.cs
@@ -4,15 +4,53 @@ using UnityEngine;
 
 public class CraftTableGUI : MonoBehaviour
 {
+
+    bool interactible = false;
+    public GameObject player;
+
+    Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = collision.gameObject;
+            interactible = true;
+        }
+
+
+
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            player = null;
+            interactible = false;        
+        }
+
+
+
+    }
+
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.F) && interactible)
+        {
+         
+            Debug.Log("Interaction with CraftTable");
+
+            GameObject go = GameObject.Find("CraftTableInventory");
+            go.SetActive(true);
+
+
+        }
     }
 }
