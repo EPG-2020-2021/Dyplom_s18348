bebb2cd754fc497bf6f1d8ba238a466b891e38bf
Tnxah <tnxah.j@gmail.com>
2021-12-29 17:07:15 +0100

    refactored some things

---
+++ Plague/Assets/Scripts/Refactoring/Object.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Object : MonoBehaviour, IInteractible, IPickable
+{
+    public new string name;
+    public int cost;
+
+
+
+
+
+
+
+
+
+
+    public void Interact()
+    {
+        pickUp();
+    }
+
+    public void pickUp()
+    {
+        
+    }
+}
