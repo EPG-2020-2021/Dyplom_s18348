6aa48ae600fdd89888e4b897e458fd2bd1aeceea
Tnxah <tnxah.j@gmail.com>
2022-06-27 15:13:53 +0200

    commit

--- Plague/Assets/Scripts/DropdownEditor.cs
+++ Plague/Assets/Scripts/DropdownEditor.cs
@@ -1,20 +1,20 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
-using UnityEngine;
+﻿//using System.Collections;
+//using System.Collections.Generic;
+//using UnityEditor;
+//using UnityEngine;
 
-[CustomEditor(typeof(DropDown))]
-public class DropdownEditor : Editor
-{
-    public override void OnInspectorGUI()
-    {
-        base.OnInspectorGUI();
+//[CustomEditor(typeof(DropDown))]
+//public class DropdownEditor : Editor
+//{
+//    public override void OnInspectorGUI()
+//    {
+//        base.OnInspectorGUI();
 
-        DropDown script = (DropDown)target;
+//        DropDown script = (DropDown)target;
 
-        GUIContent arrayLabel = new GUIContent("MyArray");
-        script.arrayidx = EditorGUILayout.Popup(arrayLabel, script.arrayidx, script.MyArray);
+//        GUIContent arrayLabel = new GUIContent("MyArray");
+//        script.arrayidx = EditorGUILayout.Popup(arrayLabel, script.arrayidx, script.MyArray);
 
 
-    }
-}
+//    }
+//}
